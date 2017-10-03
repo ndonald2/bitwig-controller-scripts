84aa1f0068e1f85b435bed0d27773d521d6dc539
Nick Donaldson <ndonald2@gmail.com>
2017-10-03 19:28:33 -0400

    Proof of concept note lighting

--- Novation/Launch Control XL/DrumMachine.js
+++ Novation/Launch Control XL/DrumMachine.js
@@ -13,12 +13,32 @@ function DrumMachine()
 
     this.drumPadBank = this.cursorDrumDevice.createDrumPadBank(8);
 
-    this.faders = this.addControl(new ControlGroup(Layout[DRUM_CHANNEL].faders.map(function (message, index)
+    var layout = Layout[DRUM_CHANNEL];
+
+    this.faders = this.addControl(new ControlGroup(layout.faders.map(function (message, index)
     {
         var encoder = new Encoder(message);
         var channel = this.drumPadBank.getChannel(index);
         encoder.connectParameter(channel.getVolume());
         return encoder;
+    }.bind(this))));
+
+    // Proof of concept note lighting
+
+    this.drumPadBank.addChannelScrollPositionObserver(function (pos) {
+      this.drumScrollOffset = pos;
+    }.bind(this), -1);
+
+    this.noteButtons = this.addControl(new ControlGroup(layout.buttons[0].map(function (message, index)
+    {
+        var button = new Button(message);
+        var channel = this.drumPadBank.getChannel(index);
+        channel.addNoteObserver(function (on, key, velocity) {
+            if (key - this.drumScrollOffset == index) {
+              button.emit('midi', message.hexByteAt(0), message.hexByteAt(1), on == true ? 0x3E : 0x00);
+            }
+        }.bind(this));
+        return button;
     }.bind(this))));
 
     // Temporary hack to select drum track in cursor (assume is first track)
