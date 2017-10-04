ee0f59eace8e40fc910bc1dc17256c332e65898f
Nick Donaldson <ndonald2@gmail.com>
2017-10-03 20:56:47 -0400

    Stable drum rack discovery

--- Novation/Launch Control XL/DrumMachine.js
+++ Novation/Launch Control XL/DrumMachine.js
@@ -1,17 +1,25 @@
 var DRUM_CHANNEL = 0;
-var DRUM_TRACK_ID = "LCXLDrumMachine"
+var DRUM_TRACK_ID = "LCXLDrumMachine";
 
 function DrumMachine()
 {
     ControlGroup.call(this);
 
+    this.selectedChannel = 0;
+    this.on('selectedChannelChanged', this.selectedChannelChanged.bind(this));
+
     this.trackBank = host.createMainTrackBank(8, 0, 0);
+    this.trackBank.addChannelCountObserver(this.updateDrumTrackConnection.bind(this));
 
     this.cursorTrack = host.createCursorTrack(DRUM_TRACK_ID, "DrumMachine", 0, 0, false);
+    this.cursorTrack.exists().addValueObserver(function (exists) {
+        if (!exists) { this.updateDrumTrackConnection(); }
+    }.bind(this));
 
     this.cursorDrumDevice = this.cursorTrack.createCursorDevice("Primary");
 
     this.drumPadBank = this.cursorDrumDevice.createDrumPadBank(8);
+    this.drumPadBank.exists().addValueObserver(this.drumPadsExistChanged.bind(this));
 
     var layout = Layout[DRUM_CHANNEL];
 
@@ -41,12 +49,31 @@ function DrumMachine()
         return button;
     }.bind(this))));
 
-    // Temporary hack to select drum track in cursor (assume is first track)
+    // Retry finding drum device when template activated
+
     this.on('activeChanged', function(active) {
-        if (active) {
-            this.cursorTrack.selectChannel(this.trackBank.getChannel(0));
-        }
+        if (active) { this._updateDrumTrackConnection(); }
     }.bind(this));
 }
 
 util.inherits(DrumMachine, ControlGroup);
+
+DrumMachine.prototype.selectedChannelChanged = function(index) {
+    this.cursorTrack.selctChannel(this.trackBank.getChannel(index));
+};
+
+DrumMachine.prototype.updateDrumTrackConnection = function() {
+    if (!this.connected) {
+        console.log("Drum track not connected, trying selected channel: " + this.selectedChannel);
+        this.cursorTrack.selectChannel(this.trackBank.getChannel(this.selectedChannel));
+    }
+};
+
+DrumMachine.prototype.drumPadsExistChanged = function (exists) {
+    this.set('connected', exists);
+    if (exists) {
+        console.log("Drum device found on channel " + this.selectedChannel);
+    } else {
+        console.log("Drum device not found on channel " + this.selectedChannel);
+    }
+};
